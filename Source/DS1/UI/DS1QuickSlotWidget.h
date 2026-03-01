af54f465cf8cb2228903a35eb527213159b7ec64
TaeseungKang-besthumor <multiloder@gmail.com>
2026-03-01 23:29:55 +0900

    풀 땡긴 내용 머지
    풀 땡긴 친구들 머지..

---
+++ Source/DS1/UI/DS1QuickSlotWidget.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/DS1PotionWidget.h"
+#include "DS1QuickSlotWidget.generated.h"
+
+UCLASS()
+class DS1_API UDS1QuickSlotWidget : public UDS1PotionWidget
+{
+	GENERATED_BODY()
+};
+
